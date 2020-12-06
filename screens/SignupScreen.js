9b0643168360fbc9bb71d2b36946aaed890c916b
kharioki <antolisah@gmail.com>
2020-12-06 22:25:22 +0300

    signup screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,19 +1,104 @@
-import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import SocialButton from '../components/SocialButton';
 
 export default function SignupScreen({navigation}) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Signup Screen</Text>
-      <Button title="click here" onPress={() => alert('Button Clicked!!!')} />
+      <Text style={styles.text}>Create an Account</Text>
+      <FormInput
+        labelValue={email}
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        placeholderText="Email"
+        iconType="user"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <FormInput
+        labelValue={password}
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        placeholderText="Password"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+      <FormButton 
+        buttonTitle="Sign Up"
+        onPress={() => alert('Sign Up Clicked!')}
+      />
+
+      <View style={styles.textPrivate}>
+        <Text style={styles.color_textPrivate}>By registering, you confirm that you accept our </Text>
+        <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
+          <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>Terms of Service</Text>
+        </TouchableOpacity>
+        <Text style={styles.color_textPrivate}> and </Text>
+        <TouchableOpacity onPress={() => alert('Policy Clicked!')}>
+          <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>Privacy Policy</Text>
+        </TouchableOpacity>
+      </View>
+
+      <SocialButton 
+        buttonTitle="Sign In with Facebook"
+        btnType="facebook"
+        color="#4867aa"
+        backgroundColor="#e6eaf4"
+        onPress={() => {}}
+      />
+
+      <SocialButton 
+        buttonTitle="Sign In with Google"
+        btnType="google"
+        color="#de4d41"
+        backgroundColor="#f5e7ea"
+        onPress={() => {}}
+      />
+
+      <TouchableOpacity style={styles.forgotButton} onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.navButtonText}>Don't have an account? Click here.</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 50
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 35,
+    justifyContent: 'center',
+  },
+  color_textPrivate: {
+    fontSize: 13,
+    fontWeight: '400',
+    fontFamily: 'Lato-Regular',
+    color: 'grey',
   },
 });
