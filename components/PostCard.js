11c7c175a99c1a32bd1b834e176afcd1f15f879b
kharioki <antolisah@gmail.com>
2021-02-24 13:39:47 +0300

    postcard component and posts

---
+++ components/PostCard.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { 
+    Card, 
+    UserInfo, 
+    UserImg, 
+    UserInfoText, 
+    UserName, 
+    PostTime, 
+    PostText, 
+    PostImg, 
+    InteractionWrapper, 
+    Interaction, 
+    InteractionText,
+    Divider
+} from '../styles/FeedStyles';
+
+const PostCard = ({ item }) => {
+    const likedIcon = item.liked ? 'heart' : 'heart-outline';
+    const likedIconColor = item.liked ? '#2e64e5' : '#333';
+    let likeText;
+    let postComments;
+
+    if(item.likes == 1) {
+        likeText = '1 Like';
+    } else if(item.likes > 1) {
+        likeText = item.likes + ' Likes';
+    } else {
+        likeText = '0 Likes';
+    }
+
+    if(item.comments == 1) {
+        postComments = '1 Comment';
+    } else if(item.comments > 1) {
+        postComments = item.comments + ' Comments';
+    } else {
+        postComments = '0 Comments';
+    }
+    return (
+        <Card>
+            <UserInfo>
+                <UserImg source={item.userImg} />
+                <UserInfoText>
+                    <UserName>{item.userName}</UserName>
+                    <PostTime>{item.postTime}</PostTime>
+                </UserInfoText>
+            </UserInfo>
+            <PostText>{item.post}</PostText>
+            {item.postImg !== 'none' ? <PostImg source={item.postImg} /> : <Divider />}
+            <InteractionWrapper>
+                <Interaction active={item.liked}>
+                    <Ionicons name={likedIcon} size={25} color={likedIconColor} />
+                    <InteractionText active={item.liked}>{likeText}</InteractionText>
+                </Interaction>
+                <Interaction>
+                    <Ionicons name="md-chatbubble-outline" size={25} />
+                    <InteractionText>{postComments}</InteractionText>
+                </Interaction>
+            </InteractionWrapper>
+        </Card>
+    )
+};
+
+export default PostCard;
